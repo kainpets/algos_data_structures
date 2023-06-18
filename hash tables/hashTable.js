29e78cf4d1f6b544bfbf9d80376227c9dc1ddd7f
Pawel Stepniak <pawelstepniak22@gmail.com>
2023-06-18 22:19:52 +0200

    hash table implementation

---
+++ hash tables/hashTable.js
@@ -0,0 +1,31 @@
+class HashTable {
+  constructor(size) {
+    this.data = new Array(size);
+  }
+
+  _hash(key) {
+    let hash = 0;
+    for (let i = 0; i < key.length; i++) {
+      hash = (hash + key.charCodeAt(i) * i) % this.data.length;
+    }
+    return hash;
+  }
+
+  set(key, value) {
+    let address = this._hash(key);
+    if (!this.data[adress]) {
+      this.data[address] = [];
+      this.data[address].push(key, value);
+    }
+    this.data[address].push(key, value);
+    return this.data;
+  }
+  get(key) {
+    
+  } 
+}
+
+const myHashTable = new HashTable(50);
+myHashTable.set("grapes", 10000);
+myHashTable.get("grapes");
+myHashTable.set("apples", 54)
